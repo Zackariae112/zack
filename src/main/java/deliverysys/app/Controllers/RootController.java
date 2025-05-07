d07cf835bad9e48d0405591c58aa5bcdc3214b5c
Zack <zack@Zacks-MacBook-Pro.local>
2025-05-07 22:18:54 +0100

    new service

--- src/main/java/deliverysys/app/Controllers/RootController.java
+++ src/main/java/deliverysys/app/Controllers/RootController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class RootController {
     @GetMapping("/")
+    
     public String root() {
         System.out.println("✅ Root '/' endpoint hit");
         return "App is running!";
