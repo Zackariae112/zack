dc6aee8456117109beb2a23951e4216f5f95d61b
Zack <zack@Zacks-MacBook-Pro.local>
2025-05-08 01:05:38 +0100

    fix jwtrqfltr

--- src/main/java/deliverysys/app/Security/JwtRequestFilter.java
+++ src/main/java/deliverysys/app/Security/JwtRequestFilter.java
@@ -64,6 +64,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
+        
         chain.doFilter(request, response);
     }
 } 
